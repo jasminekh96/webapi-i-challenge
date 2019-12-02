85134e2bb4ca9a1dbaa818f52c9e201168befae2
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-12-02 11:40:22 -0500

    guided project finished

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+	res.send('Hello world!');
+});
+
+server.get('/hobbits', (req, res) => {
+	const hobbits = [
+		{
+			id   : 1,
+			name : 'Samwise Gamgee',
+		},
+		{
+			id   : 2,
+			name : 'Frodo Baggins',
+		},
+	];
+	res.status(200).json(hobbits);
+});
+
+server.listen(8000, () => console.log('API running on port 8000'));
